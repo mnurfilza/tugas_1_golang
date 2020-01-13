74d06e59c7ea142ba0f69330885b02985d23b7ea
mnurfilza <choirfilza@gmail.com>
2020-01-13 16:49:40 +0700

    first commit

---
+++ main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "belajargolang/tugas1"
+
+func main() {
+	tugas1.Tugas1("Muhammad Nurfilza Abdul Choir")
+}
---
+++ tugas.go
@@ -0,0 +1,7 @@
+package tugas1
+
+import "fmt"
+
+func Tugas1(name string) {
+	fmt.Printf("nama saya adalah %s", name)
+}
